9ead4732b94aca69db1c9a7da717bfb91db11158
VictorJRochaJR <vrocha897@gmail.com>
2021-05-31 13:57:39 -0600

    margin

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -13,7 +13,7 @@ export default class Task {
     get taskTemplate() {
         return /*html*/ ` 
         <div class = "col-2">
-        <div class = "card">
+        <div class = "card mb-2">
         <p onclick = "app.taskController.deleteTask('${this.id}')">${this.task}</p>
         <input type = "checkbox" ${this.completed ? ' checked ' : ''} onclick = "app.taskController.toggleTask('${this.id}')"/>
          </div>
